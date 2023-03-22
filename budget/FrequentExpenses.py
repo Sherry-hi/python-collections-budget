dfebcca247a1346d5d77626fcbf4f96983ab3a26
Sherry Fu <shuang.fu@sas.com>
2023-03-22 07:59:45 +0000

    m1

---
+++ budget/FrequentExpenses.py
@@ -0,0 +1,12 @@
+from . import Expense
+import collections
+
+expenses = Expense.Expenses().read_expenses("data/spending_data.csv")
+spending_categories = []
+for expense in expenses.list:
+    spending_categories.append(expense.category)
+spending_counter = collections.Counter(spending_categories)
+top5 = spending_counter.most_common(5)
+categories, count = zip(*top5)
+
+    
