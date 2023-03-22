b4208419d7720c362ca05088326776c6ba97d728
Sherry Fu <shuang.fu@sas.com>
2023-03-22 15:04:11 +0000

    m2

---
+++ budget/BudgetList.py
@@ -0,0 +1,32 @@
+from . import Expense
+
+class BudgetList():
+    def __init__(self,budget):
+        self.budget =budget
+        self.sum_expenses = 0
+        self.expenses = []
+        self.sum_overages = 0
+        self.overages = []
+        
+    def append(self,item):
+        if self.sum_expenses + item < self.budget:
+            self.expenses.append(item)
+            self.sum_expenses = self.sum_expenses + item
+        else:
+            self.overages.append(item)
+            self.overages = self.overages + item
+    
+    def __len__(self):
+        return self.expenses + self.overages
+    
+def main():
+    myBudgetList = BudgetList(1200)         
+    expenses = Expense.Expenses()
+    expenses.read_expenses('data/spending_data.csv')
+    for expense in expenses.list:
+         expense.amount().append(myBudgetList)      
+    print('The count of all expenses: '+str(len(myBudgetList)))
+    
+    
+if __name__ == "__main" :
+    main()
