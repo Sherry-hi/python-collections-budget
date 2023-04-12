f490aa0b0f40bac6ac4f5f5579bc1cb03752f2f8
Sherry Fu <shuang.fu@sas.com>
2023-04-12 02:11:34 +0000

    back

--- budget/ExpenseCategories.py
+++ budget/ExpenseCategories.py
@@ -1,25 +1,11 @@
 from . import Expense
 import matplotlib.pyplot as plt
-import timeit
 
 def main():
     expenses = Expense.Expenses()
     expenses.read_expenses('data/spending_data.csv')
     divided_for_loop = expenses.categorize_for_loop()
-    divided_set_comp = expenses.categorize_set_comprehension()
-    if divided_for_loop != divided_set_comp:
-        print('Sets are NOT equal by == test')
-        
-    for a, b in zip(divided_for_loop, divided_set_comp):
-        if not (a.issubset(b) and b.issubset(a)):
-            print("Sets are NOT equal by subset test")
-    timeit.timeit(stmt= "expenses.categorize_for_loop()", setup = 
-                    ...
-                    from . import Expense
-                    expenses = Expense.Expenses()
-                    expenses.read_expenses('data/spending_data.csv')
-                     ...
-                  ,number=100000, globals=global()
-                  )
+
+
 if __name__ == "__main__":
     main()
